74269ba84bccce4e53f8ba519612b655a905dfad
Martin Petricko <matopetricko@gmail.com>
2025-05-31 13:09:30 +0200

    Add tests

--- src/Properties/Resolvers/SubtypeResolverInterface.php
+++ src/Properties/Resolvers/SubtypeResolverInterface.php
@@ -8,6 +8,9 @@ use MartinPetricko\LaravelDatabaseMail\Properties\Property;
 use phpDocumentor\Reflection\Type;
 use ReflectionProperty;
 
+/**
+ * Subtype resolver is used to resolve generics of a list properties like array or collection.
+ */
 interface SubtypeResolverInterface
 {
     public static function canResolveSubtype(ReflectionProperty $reflectionProperty, Type $type): bool;
